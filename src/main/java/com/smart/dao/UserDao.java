4a5e4fdc694771e17cc1743f3e975027d757c6fa
lxh <liuxuehai96@163.com>
2021-01-22 14:53:48 +0800

    v1.0

---
+++ src/main/java/com/smart/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.smart.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public int getMatchCount(String userName){
+        return 0;
+    }
+}
